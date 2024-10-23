98bf85580bc8bb745f0246a58cd543b407df8f07
MargheritaMottana <margherita.mottana@gmail.com>
2024-10-23 22:29:51 +0200

    - Importato comicseeder nel database seeder - Importato il model nel comicseeder - Creata nuova istanza di comics nel comicseeder - popolato e seeddato

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// model
+use App\Models\Comic;
+
 class ComicSeeder extends Seeder
 {
     /**
@@ -12,6 +15,38 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        Comic::truncate();
+
+        $comics = config('comic');
+
+        // ciclo per leggere e creare tutti i fumetti
+        foreach ($comics as $item) {
+
+            $comic = new Comic();
+
+            $comic->title = $item['title'];
+            $comic->description = $item['description'];
+            $comic->thumb = $item['thumb'];
+            /* 
+                - aggiunto una sottostringa per il prezzo
+                - la metto in float per avere un numero con la virgola
+                - prendo il valore a partire dal carattere con posizione 1 (quindi da dopo il $) 
+            */
+            $priceNumber = floatval(substr($item['price'], 1));
+            $comic->price = $priceNumber;
+            $comic->series = $item['series'];
+            $comic->sale_date = $item['sale_date'];
+            $comic->type = $item['type'];
+            /*
+                Trasformo la stringa degli artisti e degli scrittori in json
+            */
+            $jsonArtists = json_encode($item['artists']);
+            $comic->artists = $jsonArtists;
+            $jsonWriters = json_encode($item['writers']);
+            $comic->writers = $jsonWriters;
+
+            $comic->save();
+        }
     }
 }
